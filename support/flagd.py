50ff95c19824459bd448ba181994bc018d331d2f
Peter Jones <pjones@redhat.com>
2018-02-18 13:58:00 -0500

    Use select() and handle udev add events for new powermates.
    Both evdev and pyudev work with select(), and unlike asyncio I can
    understand it.

--- support/flagd.py
+++ support/flagd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """ This reads from the powermate's input and runs programs as a result """
 
@@ -6,60 +6,158 @@
 
 # XXX: this will eventually become a daemon in C
 
+import errno
 import os
-import asyncore
+import sys
+import select
 import evdev
+import pyudev
 
-class InputDeviceDispatcher(asyncore.file_dispatcher):
+class UdevMonitor:
+    """ Listen for new devices """
+    def __init__(self):
+        super().__init__()
+        self.ctx = pyudev.Context()
+        self.monitor = pyudev.Monitor.from_netlink(self.ctx)
+        self.monitor.filter_by('input')
+        self.monitor.start()
+
+    def fileno(self):
+        """ return the fileno for select """
+        return self.monitor.fileno()
+
+    def new_devices(self):
+        """ handle a new device """
+
+        while True:
+            device = self.monitor.poll(0)
+            if not device:
+                break
+            if device['ACTION'] != 'add':
+                continue
+            if 'DEVNAME' not in device:
+                continue
+            if 'ID_USB_DRIVER' not in device:
+                continue
+            if 'DEVLINKS' not in device:
+                continue
+            if device['ID_USB_DRIVER'] == 'powermate':
+                yield device
+
+class PowerMate:
     """ Listen for an input event and do stuff """
-    def __init__(self, name, device):
+    def __init__(self, name):
+        super().__init__()
         self.name = name
-        self.device = device
-        self.last_code = 0
-        self.last_type = 0
-        self.last_value = 0
-        asyncore.file_dispatcher.__init__(self, device)
+        self.device = evdev.InputDevice("/dev/%s" % (name,))
+        self.last = None
+        self.debug = False
+        print("new device %s" % (name,))
+
+    def fileno(self):
+        """ return the fileno for select """
+        return self.device.fileno()
 
     def save_last(self, event):
         """ maintain our state """
-        self.last_code = event.code
-        self.last_type = event.type
-        self.last_value = event.value
+        self.last = (event.code, event.type, event.value)
 
-    def recv(self, _=None):
-        """ receive from the device """
-        return self.device.read()
+    def read(self):
+        """ handle a new event """
+        try:
+            for event in self.device.read():
+                self.handle_event(event)
+        except OSError as e:
+            if e.errno == errno.ENODEV:
+                return True
+            else:
+                raise
+        return False
 
-    def handle_read(self):
-        for event in self.recv():
-            # skip null events, these are just noise
-            if event.code == 0 and event.type == 0 and event.value == 0:
-                continue
+    def handle_event(self, event):
+        """ handle one event """
 
-            # if the current event matches the most recent event, skip
-            if event.code == self.last_code and \
-               event.type == self.last_type and \
-               event.value == self.last_value:
-                continue
+        # skip null events, these are just noise
+        if event.code == 0 and event.type == 0 and event.value == 0:
+            return
 
-            # take action
-            if event.type == 2 and event.code == 7:
-                # knob turned
-                if event.value == -1:
-                    # left
-                    os.system("powermate %s left" % (self.name,))
-                    self.save_last(event)
-                elif event.value == 1:
-                    # right
-                    os.system("powermate %s right" % (self.name,))
-                    self.save_last(event)
-            elif event.type == 1 and event.code == 256:
-                # pressed
-                os.system("powermate %s button %s" % (self.name, event.value))
+        # if the current event matches the most recent event, skip
+        #if event.type == self.last_type and \
+        #   event.code == self.last_code and \
+        #   event.value == self.last_value:
+        if (event.code, event.type, event.value) == self.last:
+            if self.debug:
+                print("skipping %s event: type %s code %s value %s" % (
+                    self.name, event.type, event.code, event.value))
+            return
+
+        # take action
+        if event.type == 2 and event.code == 7 and event.value != 0:
+            # knob turned
+            if event.value < 0:
+                # left
+                os.system("powermate %s left %s" % (self.name, event.value))
                 self.save_last(event)
+            elif event.value > 0:
+                # right
+                os.system("powermate %s right %s" % (self.name, event.value))
+                self.save_last(event)
+        elif event.type == 1 and event.code == 256:
+            # pressed
+            os.system("powermate %s button %s" % (self.name, event.value))
+            self.save_last(event)
+        else:
+            if self.debug:
+                print("powermate %s event: type %s code %s value %s" % (
+                    self.name, event.type, event.code, event.value))
+            self.save_last(event)
+
+class PowerMateDispatcher():
+    """ A dispatcher for our devices """
+    def __init__(self):
+        super().__init__()
+        self.powermates = {}
+        self.udev = UdevMonitor()
+        self.udev_fileno = self.udev.fileno()
+
+        for dev in filter(lambda x: x.startswith("powermate"),
+                          os.listdir("/dev/")):
+            powermate = PowerMate(dev)
+            self.powermates[powermate.fileno()] = powermate
+
+    def new_powermate(self, device):
+        """ This gets called when we get a new powermate """
+        if not device:
+            return
+        for devlink in device['DEVLINKS'].split(' '):
+            if devlink.startswith('/dev/powermate'):
+                powermate = PowerMate(devlink[5:])
+                self.powermates[powermate.fileno()] = powermate
+                break
+
+    @property
+    def rlist(self):
+        """ the rlist for select """
+        return [k for k in self.powermates] + [self.udev_fileno]
+
+    def run(self):
+        """ this is our loop... """
+        while self.powermates:
+            r, _, _ = select.select(self.rlist, [], [])
+            for fileno in r:
+                if fileno == self.udev_fileno:
+                    for device in self.udev.new_devices():
+                        self.new_powermate(device)
+                else:
+                    powermate = self.powermates[fileno]
+                    if powermate.read():
+                        del self.powermates[fileno]
 
 if __name__ == "__main__":
-    for dev in filter(lambda x: x.startswith("powermate"), os.listdir("/dev/")):
-        powermate = evdev.InputDevice("/dev/%s" % (dev,))
-        InputDeviceDispatcher(dev, powermate)
-    asyncore.loop()
+    dispatcher = PowerMateDispatcher()
+
+    # If we can figure out how to add them on hotplug, we won't need this.
+    if not dispatcher.powermates:
+        sys.exit(0)
+
+    dispatcher.run()
